3249b12477145bda8d0a6e08d76c33d89b5a2a3f
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2023-05-17 16:52:14 -0300

    POST para Categoria

--- src/main/java/com/ricardo/book/services/CategoriaService.java
+++ src/main/java/com/ricardo/book/services/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return categoriaRepsitory.findAll();
 	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepsitory.save(obj);
+	}
 }
